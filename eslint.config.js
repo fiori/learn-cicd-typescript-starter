df03032438acb1a8f5c9e6ca8ce90348bf29fdf2
Fiori <fioridevelopment@gmail.com>
2025-05-28 17:45:16 +0100

    Linting

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import { defineConfig, globalIgnores } from "eslint/config";
+import pluginSecurity from "eslint-plugin-security"
 
 
 export default defineConfig([
@@ -9,8 +10,10 @@ export default defineConfig([
         "dist/",
         "coverage/"
     ]),
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+      plugins: { js, pluginSecurity },
+      extends: ["js/recommended", "pluginSecurity/recommended"] },
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.node } },
   tseslint.configs.recommended,
 ]);
 
